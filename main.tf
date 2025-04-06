20da858c5095fb3a168a221e50d4142b9c2649fc
aviat-at <akashtharindu7788@gmail.com>
2025-04-06 14:59:28 -0400

    CI/CD pipeline for Azure Function 3

--- main.tf
+++ main.tf
@@ -8,36 +8,32 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "sa" {
+resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "plan" {
   name                = "${var.function_app_name}-plan"
-  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type   = "Linux"         
-  sku_name  = "Y1"             
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_linux_function_app" "func" {
+resource "azurerm_linux_function_app" "function" {
   name                       = var.function_app_name
   resource_group_name        = azurerm_resource_group.rg.name
-  location                   = var.location
+  location                   = azurerm_resource_group.rg.location
   service_plan_id            = azurerm_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.sa.name
-  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  functions_extension_version = "~4"
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   site_config {
     application_stack {
       python_version = "3.10"
     }
-  }
-  app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME" = "python"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "function_app_url" {
-  value = "https://${azurerm_linux_function_app.func.default_hostname}/api/contact-api"
+  value = "https://${azurerm_linux_function_app.function.default_hostname}/api/contact-api"
 }
